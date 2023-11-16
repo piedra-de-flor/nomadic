0e858b82cec05ded9178542327443cd0c9bf4bc4
piedra-de-flor <xxspdlqjxx@naver.com>
2023-11-16 21:31:29 +0900

    Initial commit

---
+++ src/main/java/com/example/Triple_clone/TripleCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.Triple_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripleCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripleCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Triple_clone/TripleCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Triple_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripleCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
