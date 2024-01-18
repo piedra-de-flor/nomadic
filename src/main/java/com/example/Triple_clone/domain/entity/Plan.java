473f13d65873b4088dcf0ba75c805476a6340010
piedra-de-flor <xxspdlqjxx@naver.com>
2024-01-18 23:53:35 +0900

    feat : 여행 생성 기능을 위한 controller, entity, dto, service, repository생성

---
+++ src/main/java/com/example/Triple_clone/domain/entity/Plan.java
@@ -0,0 +1,10 @@
+package com.example.Triple_clone.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Plan {
+    @Id
+    private long id;
+}
