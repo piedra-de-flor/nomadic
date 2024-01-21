4d310c7ebeff39fecb7f7e95d143a4281154cd48
piedra-de-flor <xxspdlqjxx@naver.com>
2024-01-21 21:49:29 +0900

    feat : 여행 플래닝 서비스 / 날짜별로 계획 작성 api 추가

--- src/main/java/com/example/Triple_clone/domain/entity/DetailPlan.java
+++ src/main/java/com/example/Triple_clone/domain/entity/DetailPlan.java
@@ -4,9 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class DetailPlan {
     @Id
@@ -18,4 +22,15 @@ public class DetailPlan {
     private String location;
     private Date date;
     private String time;
+
+    @Builder
+    public DetailPlan(Plan plan, String name, String location, Date date, String time) {
+        DetailPlan.builder()
+                .plan(plan)
+                .name(name)
+                .location(location)
+                .date(date)
+                .time(time)
+                .build();
+    }
 }
