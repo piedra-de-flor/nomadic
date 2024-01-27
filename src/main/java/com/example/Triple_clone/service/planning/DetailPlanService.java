96c29f55a1629f30918dfc32ffd729f4afb20edf
piedra-de-flor <xxspdlqjxx@naver.com>
2024-01-27 15:27:30 +0900

    feat : DetailPlan에 대한 CRUD api 추가

--- src/main/java/com/example/Triple_clone/service/planning/DetailPlanService.java
+++ src/main/java/com/example/Triple_clone/service/planning/DetailPlanService.java
@@ -3,6 +3,7 @@ package com.example.Triple_clone.service.planning;
 import com.example.Triple_clone.domain.entity.DetailPlan;
 import com.example.Triple_clone.domain.entity.Plan;
 import com.example.Triple_clone.dto.planning.DetailPlanDto;
+import com.example.Triple_clone.dto.planning.DetailPlanUpdateDto;
 import com.example.Triple_clone.repository.DetailPlanRepository;
 import com.example.Triple_clone.repository.PlanRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,12 +16,45 @@ import java.util.NoSuchElementException;
 public class DetailPlanService {
     private final PlanRepository planRepository;
     private final DetailPlanRepository detailPlanRepository;
-    public DetailPlanDto createDetailPlan(DetailPlanDto detailPlanDto) {
+    public DetailPlanDto create(DetailPlanDto detailPlanDto) {
         Plan plan = planRepository.findById(detailPlanDto.planId())
                 .orElseThrow(() -> new NoSuchElementException("no plan Entity"));
 
         DetailPlan detailPlan = detailPlanDto.toEntity(plan);
         detailPlanRepository.save(detailPlan);
         return detailPlanDto;
+    }
+
+    public DetailPlanDto update(DetailPlanUpdateDto updateDto) {
+        Plan plan = planRepository.findById(updateDto.planId())
+                .orElseThrow(() -> new NoSuchElementException("no plan Entity"));
+
+        DetailPlan detailPlan = plan.getPlans().stream()
+                .filter(detail -> detail.getId() == updateDto.detailPlanId())
+                .findAny()
+                .orElseThrow(() -> new NoSuchElementException("no detail plan in this plan"));
+
+        detailPlan.update(
+                updateDto.location(),
+                updateDto.date(),
+                updateDto.time());
+
+        return new DetailPlanDto(plan.getId(),
+                detailPlan.getLocation(),
+                detailPlan.getDate(),
+                detailPlan.getTime());
+    }
+
+    public DetailPlan delete(long planId, long detailPlanId) {
+        Plan plan = planRepository.findById(planId)
+                .orElseThrow(() -> new NoSuchElementException("no plan Entity"));
+
+        DetailPlan detailPlan = plan.getPlans().stream()
+                .filter(detail -> detail.getId() == detailPlanId)
+                .findAny()
+                .orElseThrow(() -> new NoSuchElementException("no detail plan in this plan"));
+
+        detailPlanRepository.delete(detailPlan);
+        return detailPlan;
     }
 }
