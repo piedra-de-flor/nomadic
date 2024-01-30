4c7f9824439973f3c0c7df484ca3af2caba0ef4a
piedra-de-flor <xxspdlqjxx@naver.com>
2024-01-31 01:00:03 +0900

    test : AdminRecommendServiceTest 추가

---
+++ src/test/java/com/example/Triple_clone/service/recommend/admin/AdminRecommendServiceTest.java
@@ -0,0 +1,64 @@
+package com.example.Triple_clone.service.recommend.admin;
+
+import com.example.Triple_clone.domain.entity.Place;
+import com.example.Triple_clone.dto.recommend.admin.AdminRecommendCreatePlaceDto;
+import com.example.Triple_clone.dto.recommend.admin.AdminRecommendUpdatePlaceDto;
+import com.example.Triple_clone.repository.PlaceRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class AdminRecommendServiceTest {
+    @Mock
+    private PlaceRepository placeRepository;
+    @InjectMocks
+    private AdminRecommendService service;
+    @Mock
+    Place place;
+    @Mock
+    AdminRecommendCreatePlaceDto adminRecommendCreatePlaceDto;
+    @Mock
+    AdminRecommendUpdatePlaceDto adminRecommendUpdatePlaceDto;
+
+    @Test
+    void 서비스_레이어_관리자_장소_생성_테스트() {
+        when(adminRecommendCreatePlaceDto.toEntity()).thenReturn(place);
+
+        service.createPlace(adminRecommendCreatePlaceDto);
+
+        assertAll(
+                () -> verify(placeRepository, times(1)).save(place)
+        );
+    }
+
+    @Test
+    void 서비스_레이어_관리자_장소_수정_테스트() {
+        when(adminRecommendUpdatePlaceDto.placeId()).thenReturn(1L);
+        when(placeRepository.findById(1L)).thenReturn(Optional.ofNullable(place));
+
+        service.updatePlace(adminRecommendUpdatePlaceDto);
+
+        assertAll(
+                () -> verify(place, times(1)).update(null, null, null, null, null)
+        );
+    }
+
+    @Test
+    void 서비스_레이어_관리자_장소_삭제_테스트() {
+        when(placeRepository.findById(1L)).thenReturn(Optional.ofNullable(place));
+
+        service.deletePlace(1L);
+
+        assertAll(
+                () -> verify(placeRepository, times(1)).delete(place)
+        );
+    }
+}
