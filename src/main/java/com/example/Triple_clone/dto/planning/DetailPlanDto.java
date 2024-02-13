7c85afef9861deee5f28e5d8d661187695ab0280
piedra-de-flor <xxspdlqjxx@naver.com>
2024-02-13 17:37:01 +0900

    feat : DTO 유효성 검증 로직 추가

--- src/main/java/com/example/Triple_clone/dto/planning/DetailPlanDto.java
+++ src/main/java/com/example/Triple_clone/dto/planning/DetailPlanDto.java
@@ -3,12 +3,15 @@ package com.example.Triple_clone.dto.planning;
 import com.example.Triple_clone.domain.entity.DetailPlan;
 import com.example.Triple_clone.domain.entity.Plan;
 import com.example.Triple_clone.domain.vo.Location;
+import jakarta.validation.constraints.NotBlank;
 
 import java.util.Date;
 
 public record DetailPlanDto(
         long planId,
+        @NotBlank(message = "Location can not be null")
         Location location,
+        @NotBlank(message = "Date can not be null")
         Date date,
         String time
 ) {
--- src/main/java/com/example/Triple_clone/dto/planning/PlanCreateDto.java
+++ src/main/java/com/example/Triple_clone/dto/planning/PlanCreateDto.java
@@ -4,14 +4,18 @@ import com.example.Triple_clone.domain.entity.Plan;
 import com.example.Triple_clone.domain.entity.User;
 import com.example.Triple_clone.domain.vo.Partner;
 import com.example.Triple_clone.domain.vo.Style;
+import jakarta.validation.constraints.NotBlank;
 
 import java.util.Date;
 import java.util.List;
 
 public record PlanCreateDto(
         long userId,
+        @NotBlank(message = "Place can not be null")
         String place,
+        @NotBlank(message = "Start day can not be null")
         Date startDay,
+        @NotBlank(message = "End day can not be null")
         Date endDay,
         String partner,
         List<String> styles
