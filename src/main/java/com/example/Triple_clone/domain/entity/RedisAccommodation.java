67d2eeae16fbdadbfbec034ff7f08b828023dda6
piedra-de-flor <xxspdlqjxx@naver.com>
2025-02-27 21:09:09 +0900

    feat : Redis 추가

---
+++ src/main/java/com/example/Triple_clone/domain/entity/RedisAccommodation.java
@@ -0,0 +1,29 @@
+package com.example.Triple_clone.domain.entity;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+import java.time.LocalTime;
+
+@Getter
+@AllArgsConstructor
+@RedisHash(value="accommodation")
+public class RedisAccommodation {
+    @Id
+    private long id;
+    @Indexed
+    private String local;
+    private String name;
+    private double score;
+    private String category;
+    private int lentTime;
+    private long lentPrice;
+    private boolean lentStatus;
+    private LocalTime enterTime;
+    private long discountRate;
+    private long originPrice;
+    private long totalPrice;
+}
