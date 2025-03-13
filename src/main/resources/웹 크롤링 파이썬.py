a88ca7d8ca66ef719dfa827ec3eedc570b59fdb0
piedra-de-flor <xxspdlqjxx@naver.com>
2025-03-13 22:49:01 +0900

    refactor : FastAPI 서버 크롬 드라이버 요청마다 새로 만들어서 사용하도록 변경

--- src/main/resources/웹 크롤링 파이썬.py
+++ src/main/resources/웹 크롤링 파이썬.py
@@ -1,3 +1,5 @@
+import tempfile
+
 from fastapi import FastAPI, Depends, BackgroundTasks
 from sqlalchemy.orm import Session
 from selenium import webdriver
@@ -10,19 +12,17 @@ import time
 from datetime import datetime
 from database import get_db
 from model import Accommodation
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
 import logging
 
 app = FastAPI()
 
-chrome_options = Options()
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-
-
-import logging
-
 # 로깅 설정 (파일로도 저장하고 싶다면, filename="scraper.log" 추가)
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+executor = ThreadPoolExecutor(max_workers=5)  # 병렬 작업을 위한 스레드 풀
+
 
 def extract_lent_price_info(price_elements):
     """ 가격 및 시간 정보 파싱 """
@@ -36,6 +36,8 @@ def extract_lent_price_info(price_elements):
             text = texts[0].split(' ')
             price_info["price"] = int(text[1].replace(',', '').replace('원', ''))
             price_info["status"] = False
+        elif "숙소에 문의" in texts[0]: # 숙소에 문의
+            return price_info
         else:
             price_info["time"] = texts[0].replace("시간", '')
         if len(texts) == 3:
@@ -45,6 +47,7 @@ def extract_lent_price_info(price_elements):
 
     logging.info(f"[STEP 2] 파싱된 가격 정보: {price_info}")
     return price_info
+
 
 def extract_lodgment_price_info(price_elements):
     """ 가격 및 시간 정보 파싱 """
@@ -58,6 +61,8 @@ def extract_lodgment_price_info(price_elements):
             text = texts[0].split(' ')
             price_info["price"] = int(text[1].replace(',', '').replace('원', ''))
             price_info["status"] = False
+        elif "숙소에 문의" in texts[0]: # 숙소에 문의
+            return price_info
         else:
             price_info["time"] = texts[0].replace('~', '')
     if len(texts) == 3:
@@ -68,12 +73,21 @@ def extract_lodgment_price_info(price_elements):
     logging.info(f"[STEP 2] 파싱된 가격 정보: {price_info}")
     return price_info
 
-def run_selenium(location: str, db: Session):
+
+def sync_selenium_task(location: str, db: Session):
     """ 야놀자 크롤링 & MySQL 저장 """
     try:
+        chrome_options = Options()
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--headless")  # 백그라운드 실행
+        chrome_options.add_argument("--disable-dev-shm-usage")
+
+        # ✅ 실행할 때마다 새로운 디렉토리 생성
+        user_data_dir = tempfile.mkdtemp()
+        chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
         logging.info(f"[START] 크롤링 시작 - 지역: {location}")
 
-        service = Service(ChromeDriverManager().install())
+        service = Service("/usr/local/bin/chromedriver")
         driver = webdriver.Chrome(service=service, options=chrome_options)
 
         keyword = urllib.parse.quote(location)
@@ -81,8 +95,8 @@ def run_selenium(location: str, db: Session):
         logging.info(f"[INFO] 요청 URL: {url}")
 
         driver.get(url)
-        time.sleep(5)  # 페이지 로딩 대기
-
+        time.sleep(10)  # 페이지 로딩 대기
+        print(driver.page_source)
         soup = BeautifulSoup(driver.page_source, "html.parser")
         driver.quit()
 
@@ -91,14 +105,18 @@ def run_selenium(location: str, db: Session):
         logging.info(f"[INFO] 총 {len(elements)}개의 숙소 정보 발견")
 
         for element in elements:
-            name = element.select_one("div.PlaceListTitle_container__qe7XH").text.strip() if element.select_one("div.PlaceListTitle_container__qe7XH") else None
-            score = float(element.select_one("span.PlaceListScore_rating__3Glxf").text.strip()) if element.select_one("span.PlaceListScore_rating__3Glxf") else None
-            category = element.select_one("div.PlaceListGrade_container__1oIhJ").text.strip() if element.select_one("div.PlaceListGrade_container__1oIhJ") else None
+            name = element.select_one("div.PlaceListTitle_container__qe7XH").text.strip() if element.select_one(
+                "div.PlaceListTitle_container__qe7XH") else None
+            score = float(element.select_one("span.PlaceListScore_rating__3Glxf").text.strip()) if element.select_one(
+                "span.PlaceListScore_rating__3Glxf") else None
+            category = element.select_one("div.PlaceListGrade_container__1oIhJ").text.strip() if element.select_one(
+                "div.PlaceListGrade_container__1oIhJ") else None
 
             logging.info(f"[INFO] 숙소 정보: 이름={name}, 평점={score}, 카테고리={category}")
 
             price_info_elements = element.select("div.PlacePriceInfoV2_topInfo__mDkFX span")
-            price_info_texts = [BeautifulSoup(str(e), "html.parser").text.strip() for e in price_info_elements if BeautifulSoup(str(e), "html.parser").text.strip()]
+            price_info_texts = [BeautifulSoup(str(e), "html.parser").text.strip() for e in price_info_elements if
+                                BeautifulSoup(str(e), "html.parser").text.strip()]
             logging.info(f"{price_info_texts}")
 
             lent_info_elements = price_info_texts[1:price_info_texts.index("숙박")]
@@ -115,6 +133,8 @@ def run_selenium(location: str, db: Session):
             price_texts = [price.text.strip() for price in prices if price.text.strip()]
             logging.info(f"[DEBUG] 추출된 가격 텍스트 리스트: {price_texts}")
 
+            if len(price_texts) == 1:
+                price_texts.insert(0, "0")
 
             accommodation = {
                 "local": location,
@@ -125,13 +145,16 @@ def run_selenium(location: str, db: Session):
                 "lent_time": int(lent_parsed_info["time"]) if lent_parsed_info["time"] else 0,
                 "lent_origin_price": lent_parsed_info.get("origin_price") if lent_parsed_info["origin_price"] else 0,
                 "lent_price": int(price_texts[0].replace(',', '').replace('원~', '').replace('최대할인가', ''))
-                if price_texts[0] and lent_parsed_info.get("status", True) and price_texts[0] != "예약마감" else None,
+                if price_texts[0] and lent_parsed_info.get("status", True) and price_texts[0] != "예약마감" and price_texts[0] != "숙소에 문의" else 0,
                 "lent_status": lent_parsed_info.get("status", True),
-                "enter_time": datetime.strptime(lodgment_parsed_info["time"], "%H:%M").time() if lodgment_parsed_info["time"] else datetime.strptime("00:00", "%H:%M").time(),
-                "lodgment_discount_rate": lodgment_parsed_info.get("discount_rate") if lent_parsed_info["discount_rate"] else 0,
-                "lodgment_origin_price": lodgment_parsed_info.get("origin_price") if lent_parsed_info["origin_price"] else 0,
+                "enter_time": datetime.strptime(lodgment_parsed_info["time"], "%H:%M").time() if lodgment_parsed_info[
+                    "time"] else datetime.strptime("00:00", "%H:%M").time(),
+                "lodgment_discount_rate": lodgment_parsed_info.get("discount_rate") if lent_parsed_info[
+                    "discount_rate"] else 0,
+                "lodgment_origin_price": lodgment_parsed_info.get("origin_price") if lent_parsed_info[
+                    "origin_price"] else 0,
                 "lodgment_price": int(price_texts[1].replace(',', '').replace('원~', '').replace('최대할인가', ''))
-                if price_texts[1] and lodgment_parsed_info.get("status", True) and price_texts[1] != "예약마감" else None,
+                if price_texts[1] and lodgment_parsed_info.get("status", True) and price_texts[1] != "예약마감" and price_texts[1] != "숙소에 문의" else 0,
                 "lodgment_status": lodgment_parsed_info.get("status", True),
             }
 
@@ -157,9 +180,17 @@ def run_selenium(location: str, db: Session):
         logging.error(f"[ERROR] 크롤링 및 DB 저장 중 에러 발생: {str(e)}", exc_info=True)
         return {"status": "fail", "error": str(e)}
 
+
+async def run_selenium(location: str, db: Session):
+    """야놀자 크롤링 & MySQL 저장 (비동기)"""
+    loop = asyncio.get_event_loop()
+    return await loop.run_in_executor(executor, sync_selenium_task, location, db)
+
+
 @app.get("/scrape/{location}")
-def scrape_endpoint(location: str, background_tasks: BackgroundTasks, db: Session = Depends(get_db)):
-    """ 비동기 크롤링 실행 """
+async def scrape_endpoint(location: str, db: Session = Depends(get_db)):
+    """크롤링 실행"""
     logging.info(f"크롤링 요청 받음: {location}")
-    background_tasks.add_task(run_selenium, location, db)
-    return {"status": "scraping started", "location": location}
+    result = await run_selenium(location, db)  # 백그라운드 실행 없이 직접 실행
+    return result
+
