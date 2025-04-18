bcb3000ec5f25a267b602df5e2349ef4ca031447
piedra-de-flor <xxspdlqjxx@naver.com>
2025-04-18 20:16:05 +0900

    remove : 기존의 Querydsl 코드 삭제

--- src/main/java/com/example/Triple_clone/service/accommodation/AccommodationService.java
+++ src/main/java/com/example/Triple_clone/service/accommodation/AccommodationService.java
@@ -5,7 +5,6 @@ import com.example.Triple_clone.domain.entity.AccommodationDocument;
 import com.example.Triple_clone.dto.accommodation.AccommodationDto;
 import com.example.Triple_clone.repository.AccommodationRepository;
 import com.example.Triple_clone.repository.ESAccommodationRepositoryImpl;
-import com.example.Triple_clone.repository.ESRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -13,7 +12,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,35 +24,6 @@ public class AccommodationService {
     private final AccommodationRepository repository;
     private final ESAccommodationRepositoryImpl esRepository;
     private final static int PAGE_SIZE = 5;
-
-    @Transactional(readOnly = true)
-    public List<AccommodationDto> searchDB(String local,
-                                          String name,
-                                          String discountRate,
-                                          String startLentPrice,
-                                          String endLentPrice,
-                                          String category,
-                                          String score,
-                                          String lentStatus,
-                                          String enterTime,
-                                          String startLodgmentPrice,
-                                          String endLodgmentPrice,
-                                          String lodgmentStatus,
-                                          Pageable pageable) {
-        Page<Accommodation> accommodationPage;
-        Pageable customPageable = PageRequest.of(pageable.getPageNumber(), PAGE_SIZE, Sort.unsorted());
-        List<AccommodationDto> response = new ArrayList<>();
-
-        accommodationPage = repository.searchByConditionsFromDB(local, name, discountRate, startLentPrice, endLentPrice,
-                category, score, lentStatus, enterTime, startLodgmentPrice, endLodgmentPrice, lodgmentStatus, customPageable);
-
-        for (Accommodation accommodation : accommodationPage) {
-            AccommodationDto dto = new AccommodationDto(accommodation);
-            response.add(dto);
-        }
-
-        return response;
-    }
 
     public List<AccommodationDto> searchES(
             String local,
@@ -76,13 +45,6 @@ public class AccommodationService {
         Page<AccommodationDocument> documents = esRepository.searchByConditionsFromES(
                 local, name, category, discountRate, startLentPrice, endLentPrice,
                 score, lentStatus, startLodgmentPrice, endLodgmentPrice,enterTime, lodgmentStatus, customPageable);
-
-        if (documents.isEmpty()) {
-            return response;
-            /*log.info("no date in ES, search RDB");
-            return searchDB(local, name, category, discountRate, startLentPrice, endLentPrice,
-                    score, lentStatus, startLodgmentPrice, endLodgmentPrice,enterTime, lodgmentStatus, pageable);*/
-        }
 
         for (AccommodationDocument document : documents) {
             AccommodationDto dto = new AccommodationDto(document);
--- src/main/java/com/example/Triple_clone/web/controller/accommodation/AccommodationController.java
+++ src/main/java/com/example/Triple_clone/web/controller/accommodation/AccommodationController.java
@@ -1,7 +1,6 @@
 package com.example.Triple_clone.web.controller.accommodation;
 
 import com.example.Triple_clone.dto.accommodation.AccommodationDto;
-import com.example.Triple_clone.dto.accommodation.AccommodationsSaveDto;
 import com.example.Triple_clone.service.accommodation.AccommodationService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -9,7 +8,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
