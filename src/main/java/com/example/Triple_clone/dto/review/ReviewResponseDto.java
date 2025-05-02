1ba85ea509b5860ebcc5ab5dc52804cb25ccc979
piedra-de-flor <xxspdlqjxx@naver.com>
2025-05-02 21:00:51 +0900

    feat : Review 응답 DTO 추가

---
+++ src/main/java/com/example/Triple_clone/dto/review/ReviewResponseDto.java
@@ -0,0 +1,21 @@
+package com.example.Triple_clone.dto.review;
+
+import com.example.Triple_clone.domain.entity.Review;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ReviewResponseDto {
+    private Long id;
+    private String content;
+    private List<ReviewResponseDto> replies;
+
+    public ReviewResponseDto(Review review) {
+        this.id = review.getId();
+        this.content = review.getContent();
+        this.replies = review.getChildren()
+                .stream()
+                .map(ReviewResponseDto::new)
+                .collect(Collectors.toList());
+    }
+}
