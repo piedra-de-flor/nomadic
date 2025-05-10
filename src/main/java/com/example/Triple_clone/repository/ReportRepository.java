00e215324d7da5675b184bf752312a1c5c08dd8a
piedra-de-flor <xxspdlqjxx@naver.com>
2025-05-10 21:48:13 +0900

    feat : 객체지향적 변경으로 인한 Service, repository 로직 변경

--- src/main/java/com/example/Triple_clone/repository/ReportRepository.java
+++ src/main/java/com/example/Triple_clone/repository/ReportRepository.java
@@ -3,11 +3,6 @@ package com.example.Triple_clone.repository;
 import com.example.Triple_clone.domain.entity.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface ReportRepository extends JpaRepository<Report, Long> {
-
-    boolean existsByReviewIdAndReporterId(Long commentId, Long reporterId);
-
-    Optional<Report> findByReviewIdAndReporterId(Long commentId, Long reporterId);
+    boolean existsByTargetTypeAndTargetIdAndReporterId(String targetType, Long targetId, Long reporterId);
 }
