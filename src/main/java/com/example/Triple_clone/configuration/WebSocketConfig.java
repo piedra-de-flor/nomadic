4036691add939e65b56d26dd489b480097a73b43
piedra-de-flor <xxspdlqjxx@naver.com>
2025-08-13 20:13:06 +0900

    feat : websocket config 구현 및 핸들러, DTO 선언

---
+++ src/main/java/com/example/Triple_clone/configuration/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.example.Triple_clone.configuration;
+
+import com.example.Triple_clone.domain.plan.websocket.PlanWebSocketHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final PlanWebSocketHandler planWebSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(planWebSocketHandler, "/ws/plan/{planId}")
+                .setAllowedOrigins("*")
+                .withSockJS();
+    }
+}
---
+++ src/main/java/com/example/Triple_clone/domain/plan/application/PlanWebSocketHandler.java
@@ -0,0 +1,4 @@
+package com.example.Triple_clone.domain.plan.application;
+
+public class PlanWebSocketHandler {
+}
