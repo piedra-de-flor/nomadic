09fd7f6b0115c2ddb8caefcbf71adecf3d00c93a
piedra-de-flor <xxspdlqjxx@naver.com>
2025-08-18 22:34:31 +0900

    feat : PlanWebSocketHandler Spring Bean으로 등록 및 TextWebSocketHandler상속

--- src/main/java/com/example/Triple_clone/configuration/WebSocketConfig.java
+++ src/main/java/com/example/Triple_clone/configuration/WebSocketConfig.java
@@ -1,6 +1,6 @@
 package com.example.Triple_clone.configuration;
 
-import com.example.Triple_clone.domain.plan.websocket.PlanWebSocketHandler;
+import com.example.Triple_clone.domain.plan.application.PlanWebSocketHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
--- src/main/java/com/example/Triple_clone/domain/plan/application/PlanWebSocketHandler.java
+++ src/main/java/com/example/Triple_clone/domain/plan/application/PlanWebSocketHandler.java
@@ -1,4 +1,8 @@
 package com.example.Triple_clone.domain.plan.application;
 
-public class PlanWebSocketHandler {
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class PlanWebSocketHandler extends TextWebSocketHandler {
 }
