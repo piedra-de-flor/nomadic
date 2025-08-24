a533674843897db728c7771a2cf89149e7cb6e82
piedra-de-flor <xxspdlqjxx@naver.com>
2025-08-24 23:07:40 +0900

    feat : plan 관련 이벤트 DTO 클래스 생성

---
+++ src/main/java/com/example/Triple_clone/domain/plan/web/dto/event/plan/PlanCreatedEvent.java
@@ -0,0 +1,27 @@
+package com.example.Triple_clone.domain.plan.web.dto.event.plan;
+
+import com.example.Triple_clone.domain.member.domain.Member;
+import com.example.Triple_clone.domain.plan.domain.ChangeType;
+import com.example.Triple_clone.domain.plan.domain.Plan;
+import com.example.Triple_clone.domain.plan.domain.TargetType;
+import com.example.Triple_clone.domain.plan.web.dto.PlanCreateChangeData;
+import com.example.Triple_clone.domain.plan.web.dto.event.PlanChangeEvent;
+import lombok.Getter;
+
+@Getter
+public class PlanCreatedEvent extends PlanChangeEvent {
+    public PlanCreatedEvent(Object source, Plan plan, Member changedBy) {
+        super(source, plan, changedBy, ChangeType.CREATED, plan.getId(), TargetType.PLAN);
+    }
+
+    @Override
+    public Object getChangeData() {
+        return PlanCreateChangeData.builder()
+                .place(getPlan().getPlace())
+                .partner(getPlan().getPartner() != null ? getPlan().getPartner().name() : null)
+                .styles(getPlan().getStyles().stream().map(Enum::name).collect(java.util.stream.Collectors.toList()))
+                .startDay(getPlan().getStartDay())
+                .endDay(getPlan().getEndDay())
+                .build();
+    }
+}
